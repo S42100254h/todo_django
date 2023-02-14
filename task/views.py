a0bec6f0079729b83367f7f6ebb217cf608ea5dc
yuki sawabe <sawabe0v0@gmail.com>
2023-02-14 17:17:12 +0900

    update TaskView

--- task/views.py
+++ task/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import redirect, render
 from django.views.generic import View
+from django.http.response import JsonResponse
 
 from .forms import TaskForm
 from .models import Task
 
+
 class TaskView(View):
     def get(self, request):
-        params = {}
-        params["task"] = Task.objects.all()
-        params["form"] = TaskForm()
-        return render(request, "task_list.html", params)
+        if request.headers.get("Content-Type") == "application/json":
+            tasks = Task.objects.values()
+            tasks_list = list(tasks)
+            return JsonResponse(tasks_list, safe=False, status=200)
+        return render(request, "task_list.html")
 
     def post(self, request):
         form = TaskForm(request.POST)

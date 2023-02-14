56422495be767852fd81ffb25b03781e74c970e5
yuki sawabe <sawabe0v0@gmail.com>
2023-02-14 17:00:22 +0900

    add getTask method

--- task/static/main.js
+++ task/static/main.js
@@ -7,8 +7,31 @@ const App = {
   compilerOptions: {
     delimiters: ["[[", "]]"],
   },
-  methods: {},
-  created() {},
+  methods: {
+    getTasks() {
+      fetch(URL, {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((tasks_list) => {
+          this.tasks = tasks_list;
+        })
+        .catch((error) => {
+          console.error(
+            "There has been a problem with your fetch operation:",
+            error
+          );
+        });
+    },
+  },
+  created() {
+    this.getTasks();
+  },
 };
 
 Vue.createApp(App).mount("#app");
